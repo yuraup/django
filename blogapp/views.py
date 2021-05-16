23c30cbb151d2012776ded768a43ac2fc803584b
yura <o721@naver.com>
2021-05-16 20:38:17 +0900

    blog

---
+++ blogapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+def home(request):
+  blogs = Blog.objects
+  return render(request, 'home.html', {'blogs' : blogs})
+
+def detail(request, blog_id):
+    details = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html', {'details' :details})
+
+
